acc04731872eb6337323519ce8bcdea40048da43
Lukayx <olavarriafernando36@gmail.com>
2021-07-07 11:50:12 -0400

    Con pantalla de carga

--- Cosas_nesesarias_para_nuestro_juego/Pantalla de carga.py
+++ Cosas_nesesarias_para_nuestro_juego/Pantalla de carga.py
@@ -9,6 +9,14 @@ def Pantalla_carga(ventana):
     f_muro = 0
     background=pygame.image.load((os.path.dirname(os.path.abspath(__file__))+"\\Background\P_CARGA.png"))
     background = pygame.transform.scale(background,(960,600))
+    cabeza=[pygame.image.load(os.path.dirname(os.path.abspath(__file__))+"\\Sprites\C1.png").convert_alpha(),
+            pygame.image.load(os.path.dirname(os.path.abspath(__file__))+"\\Sprites\C2.png").convert_alpha(),
+            pygame.image.load(os.path.dirname(os.path.abspath(__file__))+"\\Sprites\C3.png").convert_alpha(),
+            pygame.image.load(os.path.dirname(os.path.abspath(__file__))+"\\Sprites\C4.png").convert_alpha(),
+            pygame.image.load(os.path.dirname(os.path.abspath(__file__))+"\\Sprites\C5.png").convert_alpha(),
+            pygame.image.load(os.path.dirname(os.path.abspath(__file__))+"\\Sprites\C6.png").convert_alpha(),
+            pygame.image.load(os.path.dirname(os.path.abspath(__file__))+"\\Sprites\C7.png").convert_alpha(),
+            pygame.image.load(os.path.dirname(os.path.abspath(__file__))+"\\Sprites\C8.png").convert_alpha()]
     run=True
     while run:
         ventana.fill((0,0,0))
@@ -20,6 +28,7 @@ def Pantalla_carga(ventana):
         if(f_muro==60):
             fotograma+=1
             f_muro=0
-        if(fotograma==2):
+        if(fotograma==7):
             fotograma=0
+        ventana.blit(cabeza[fotograma],[960-100,800-200])
         pygame.display.flip()
